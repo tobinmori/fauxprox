509c6d5bda4e3754b4a183383265b6feb147296d
Tobin Mori <tobinmori@gmail.com>
2013-05-30 21:58:27 -0400

    more msgs

--- messages.py
+++ messages.py
@@ -1,13 +1,15 @@
 
 
-messages = { 'default': 'Running process.',
-             'funny' : 'I am busy now! Leave me alone',
-             'configuring' : 'Configuring',
-             'extracting' : 'Extracting',
-             'compressing' : 'Compressing',
-             'deleting' : 'Destroying everything',
+messages = { 'default': 'Running process. . .',
+             'funny' : 'I am busy now! Leave me alone!',
+             'configuring' : 'Configuring something. . .',
+             'extracting' : 'Extracting stuff. . .',
+             'compressing' : 'Compressing more stuff. . .',
+             'deleting' : 'Destroying everything.',
              'formatting': 'Formatting hardrive. . .',
              'faxing': 'Faxing TPS-Report. . .',
              'cover sheet': 'Adding cover sheet. . .',
              'installing': 'Installing Windows 2000. . .',
-             'warming up': 'Warming up dot matrix printer. . .'}
+             'warming up': 'Warming up dot matrix printer. . .'
+             'organizing': 'Organizing vacation photos. . .',
+}
